a56e3367268b06d2814e9301499e5abb4db9c600
Andrew Hickman <andrew.hickman1@sky.com>
2017-04-24 03:45:21 +0100

    do lifetime stuff

--- src/typeck/mod.rs
+++ src/typeck/mod.rs
@@ -2,10 +2,7 @@ use std::collections::HashMap;
 
 use ast;
 
-#[derive(Default, PartialEq, Clone, Debug, Eq)]
-pub struct SymbolTable<'a> {
-    names: HashMap<&'a str, Ty>,
-}
+pub type SymbolTable<'a> = HashMap<&'a str, Ty>;
 
 #[derive(PartialEq, Eq, Copy, Clone, Debug)]
 pub enum Ty {
@@ -14,41 +11,46 @@ pub enum Ty {
     Bool,
 }
 
-pub fn typeck(ast: &mut ast::Ast) -> Result<(), String> {
-    typeck_scope(&mut ast.main)
+pub struct TyChecker<'b, 'a: 'b> {
+    symbols: Vec<&'b mut SymbolTable<'a>>,
 }
 
-fn typeck_scope<'a>(scope: &mut ast::Scope<'a>) -> Result<(), String> {
-    use ast::Statement::*;
-    for statement in &mut scope.statements {
-        match *statement {
-            Declaration(ref decl) => {
-                if scope.symbols.names.insert(decl.name, decl.ty).is_some() {
-                    return Err(format!("variable with name {} already declared", decl.name));
-                }
-                if let Some(ref expr) = decl.expr {
-                    let ty = ty_of(expr)?;
-                    if ty != decl.ty {
-                        return Err(format!("type mismatch: variable {} has type {:?} but expression \
-                                            {} has type {:?}", decl.name, decl.ty, expr, ty));
+impl<'b, 'a: 'b> TyChecker<'b, 'a> {
+    pub fn ast(&mut self, ast: &'b mut ast::Ast<'a>) -> Result<(), String> {
+        self.scope(&mut ast.main)
+    }
+
+    fn scope(&mut self, scope: &'b mut ast::Scope<'a>) -> Result<(), String> {
+        use ast::Statement::*;
+        self.symbols.push(&mut scope.symbols);
+        for statement in &mut scope.statements {
+            match *statement {
+                Declaration(ref decl) => {
+                    if self.symbols.last_mut().unwrap().insert(decl.name, decl.ty).is_some() {
+                        return Err(format!("variable with name {} already declared", decl.name));
+                    }
+                    if let Some(ref expr) = decl.expr {
+                        let ty = self.expr(expr)?;
+                        if ty != decl.ty {
+                            return Err(format!("type mismatch: variable {} has type {:?} but \
+                                                expression {} has type {:?}", decl.name, decl.ty, 
+                                                expr, ty));
+                        }
                     }
                 }
+                Expression(ref expr) => {
+                    self.expr(expr)?;
+                },
+                Scope(ref mut scope) => {
+                    self.scope(scope)?;
+                },
             }
-            Expression(ref expr) => {
-                typeck_expr(expr, &mut scope.symbols)?;
-            },
-            Scope(ref mut scope) => {
-                typeck_scope(scope)?;
-            },
         }
+        Ok(())
     }
-    Ok(())
+
+    fn expr(&mut self, expr: &ast::Expr<'a>) -> Result<Ty, String> {
+        unimplemented!()
+    }
 }
 
-fn typeck_expr<'a>(expr: &ast::Expr<'a>, symbols: &mut SymbolTable<'a>) -> Result<(), String> {
-    unimplemented!()
-}
-
-fn ty_of<'a>(expr: &ast::Expr<'a>) -> Result<Ty, String> {
-    unimplemented!()
-}
